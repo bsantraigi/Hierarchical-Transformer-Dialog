675ab8e758a50d6813fa4f1305e42f8194831229
Bishal Santra <bsantraigi@gmail.com>
2020-09-16 19:52:29 +0530

    minor: print args in main + jsob_main

---
+++ sjob_main.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH -J main_best_hier            #Job name(--job-name)
+#SBATCH -o logs/slurm_%j.log          #Name of stdout output file(--output)
+#SBATCH -e logs/slurm_%j.log		   #Name of stderr error file(--error)
+#SBATCH -p gpu              #Queue (--partition) name 
+#SBATCH -n 1                    #Total Number of mpi tasks (--ntasks .should be 1 for serial)
+#SBATCH -c 1                    #(--cpus-per-task) Number of Threads
+#SBATCH --mem=23000        # Memory per node specification is in MB. It is optional. 
+#SBATCH --mail-user=bsantraigi@gmail.com        # user's email ID where job status info will be sent
+#SBATCH --mail-type=ALL        # Send Mail for all type of event regarding the job
+pwd; hostname; date
+
+module load compiler/intel-mpi/mpi-2019-v5
+module load compiler/cuda/10.1
+
+# source /home/$USER/.bashrc
+
+export CUDA_VISIBLE_DEVICES=0,1
+mpirun -bootstrap slurm which python
+mpirun -bootstrap slurm nvcc --version
+mpirun -bootstrap slurm python main.py -embed 96 -heads 4 -hid 64 -l_e1 4 -l_e2 2 -l_d 2 -d 0.2024 -bs 8 -e 30 -model HIER
