43cbe00b168cf1847c651f99a2539ae305a0c445
Bishal Santra <bsantraigi@gmail.com>
2020-10-03 16:29:36 +0530

    minor changes in job scripts

--- sjob_search_acts.sh
+++ sjob_search_acts.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #SBATCH -J hier_search            #Job name(--job-name)
-#SBATCH -o logs/slurm.log          #Name of stdout output file(--output)
-#SBATCH -e logs/slurm.log		   #Name of stderr error file(--error)
+#SBATCH -o logs/slurm_%j.log          #Name of stdout output file(--output)
+#SBATCH -e logs/slurm_%j.log		   #Name of stderr error file(--error)
 #SBATCH -p gpu              #Queue (--partition) name 
 #SBATCH -n 1                    #Total Number of mpi tasks (--ntasks .should be 1 for serial)
 #SBATCH -c 1                    #(--cpus-per-task) Number of Threads
 #SBATCH --mem=23000        # Memory per node specification is in MB. It is optional. 
 #SBATCH --mail-user=bishal.santra@iitkgp.ac.in        # user's email ID where job status info will be sent
 #SBATCH --mail-type=ALL        # Send Mail for all type of event regarding the job
+#SBATCH --time 1-0
 
 module load compiler/intel-mpi/mpi-2019-v5
 module load compiler/cuda/10.1
